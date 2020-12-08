d984800e5945cfb5a3b1b18fc6bdf376c1344776
Vicente Paulo de Souza Júnior <59590810+vicentepaulo@users.noreply.github.com>
2020-12-08 15:47:28 -0300

    commit

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Luna',
+                partido: 'Piticão',
+                fotos: [{
+                    url: 'luna.jpg', legenda: 'Vereador'
+                }
+                ]
+            },
+            {
+                numero: '87144',
+                nome: 'Bibi',
+                partido: 'CatioroBrabo',
+                fotos: [{
+                    url: '91_judo.jpg', legenda: ''
+                }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'FULANO',
+                partido: 'ABC',
+                vice: 'FULANO',
+                fotos: [
+                    { url: '91_judo.jpg', legenda: 'Prefeito' },
+                    { url: '91_judo.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '98',
+                nome: 'Fulano',
+                partido: 'NCA',
+                vice: 'FULANO',
+                fotos: [
+                    { url: '91_judo.jpg', legenda: 'Prefeito' },
+                    { url: '91_judo.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
